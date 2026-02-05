ca277266a4247f79eb3342c3a83c7480752a11e7
Brett Egbert <brett@crossinguard.dev>
2026-02-05 12:11:06 -0800

    update nav styles/organization; add back astro icons and page icons in visitor guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    icon({
+      iconDir: "src/assets/icons",
+    }),
+  ],
 });
